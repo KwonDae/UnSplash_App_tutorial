0c74c41588dc9676640023b0f9def9e388fd04df
daewon.kwon <green201402317@gmail.com>
2021-12-01 21:01:11 +0900

    #14 searchView

--- app/src/main/java/com/example/unsplash_app_tutorial/PhotoCollectionActivity.kt
+++ app/src/main/java/com/example/unsplash_app_tutorial/PhotoCollectionActivity.kt
@@ -1,21 +1,37 @@
 package com.example.unsplash_app_tutorial
 
+import android.app.SearchManager
+import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
+import android.text.InputFilter
 import android.util.Log
+import android.view.Menu
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.unsplash_app_tutorial.model.Photo
 import com.example.unsplash_app_tutorial.recyclerview.PhotoGridRecyclerViewAdapter
 import com.example.unsplash_app_tutorial.utils.Constants.TAG
 import kotlinx.android.synthetic.main.activity_photo_collection.*
 
-class PhotoCollectionActivity : AppCompatActivity() {
+class PhotoCollectionActivity : AppCompatActivity(),
+                                    SearchView.OnQueryTextListener
+{
 
     // 데이터
     private var photoList = ArrayList<Photo>()
 
     // 어답터
     private lateinit var photoGridRecyclerViewAdapter: PhotoGridRecyclerViewAdapter
+
+    // 서치뷰
+    private lateinit var mySearchView: SearchView
+
+    // 서치뷰 에딧 텍스트
+    private lateinit var mySearchViewEditText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +44,9 @@ class PhotoCollectionActivity : AppCompatActivity() {
 
         top_app_bar.title = searchTerm
 
+        // 액티비티에서 어떤 액션바를 사용할지 설정한다.
+        setSupportActionBar(top_app_bar)
+
         this.photoGridRecyclerViewAdapter = PhotoGridRecyclerViewAdapter(null)
 
         this.photoGridRecyclerViewAdapter.submitList(photoList)
@@ -37,5 +56,75 @@ class PhotoCollectionActivity : AppCompatActivity() {
 
         Log.d(TAG, "PhotoCollectionActivity - onCreate() called / searchTerm : $searchTerm, photoList.count() : ${photoList.size}")
 
+    }// onCreate
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        Log.d(TAG, "PhotoCollectionActivity - onCreateOptionsMenu called / ");
+        val inflater = menuInflater
+        inflater.inflate(R.menu.top_app_bar_menu, menu)
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+
+        this.mySearchView = menu?.findItem(R.id.search_menu_item)?.actionView as SearchView
+
+        this.mySearchView.apply {
+            this.queryHint = "검색어를 입력해주세요"
+
+            this.setOnQueryTextListener(this@PhotoCollectionActivity)
+
+            this.setOnQueryTextFocusChangeListener { _, hasExpaned ->
+                when(hasExpaned) {
+                    true -> {
+                        Log.d(TAG, "서치뷰 열림");
+                    }
+                    false -> {
+                        Log.d(TAG, "서치뷰 닫힘")
+                    }
+                }
+            }
+            // 서치뷰에서 EditText를 가져온다
+            mySearchViewEditText = this.findViewById(androidx.appcompat.R.id.search_src_text)
+        }
+
+        this.mySearchViewEditText.apply {
+            this.filters = arrayOf(InputFilter.LengthFilter(12))
+            this.setTextColor(Color.WHITE)
+            this.setHintTextColor(Color.WHITE)
+        }
+
+        return true
+    }
+
+    // 서치뷰 검색어 입력 이벤트
+    // 검색버튼이 클릭되었을 때
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        Log.d(TAG, "PhotoCollectionActivity - onQueryTextSubmit called / query: $query")
+
+        if(!query.isNullOrEmpty()) {
+            this.top_app_bar.title = query
+
+            // TODO: 2021/12/01 daengdaeng : api 호출
+            // TODO:: 검색어 저장
+        }
+
+//        this.mySearchView.setQuery("", false)
+//        this.mySearchView.clearFocus()
+        this.top_app_bar.collapseActionView()
+
+        return true
+    }
+
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        Log.d(TAG, "PhotoCollectionActivity - onQueryTextChange called / newTezt: $newText")
+        
+        val userInputText = newText.let {
+            it
+        }?: ""
+        
+        if(userInputText.count() == 12) {
+            Toast.makeText(this, "검색어는 12자 까지만 입력 가능합니다.", Toast.LENGTH_SHORT).show()
+        }
+        return true
     }
 }
